5acea4c3b5c382d23e011c5c55308e80cd12eb68
Niels Koster <n.koster@portavita.eu>
2018-07-02 17:41:29 +0200

    Use class

--- simple.py
+++ simple.py
@@ -1,6 +1,12 @@
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-window = Gtk.Window()
+class MainWndow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Title")
+
+window = MainWndow()
 window.connect('delete-event', Gtk.main_quit)
 window.show_all()
 Gtk.main()
