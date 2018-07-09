8a0b58b466bc718d7627f13a64c9578d92b4c0c4
Niels Koster <n.koster@portavita.eu>
2018-07-09 21:30:35 +0200

    Initial, Gtk skeleton file

---
+++ skeleton.py
@@ -0,0 +1,12 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class MainWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Main Window")
+
+win = MainWindow()
+win.connect('delete-event', Gtk.main_quit)
+win.show_all()
+Gtk.main()
