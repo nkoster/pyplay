a64c8b1eed58cf73cc46f1a35fa400f52435e254
Niels Koster <n.koster@portavita.eu>
2018-07-10 00:56:25 +0200

    Initial, header bar

---
+++ headerbar.py
@@ -0,0 +1,41 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, Gio
+
+
+class MainWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Header Bar")
+        self.set_border_width(10)
+        self.set_default_size(500, 1)
+
+        header_bar = Gtk.HeaderBar()
+        header_bar.set_show_close_button(True)
+        header_bar.props.title = "Some Stuff"
+
+        audio_button = Gtk.Button()
+        cd_icon = Gio.ThemedIcon(name="gnome-dev-cdrom-audio")
+        image = Gtk.Image.new_from_gicon(cd_icon, Gtk.IconSize.BUTTON)
+        audio_button.add(image)
+        header_bar.pack_end(audio_button)
+
+        box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        Gtk.StyleContext.add_class(box.get_style_context(), 'linked')
+
+        left_arrow = Gtk.Button()
+        left_arrow.add(Gtk.Arrow(Gtk.ArrowType.LEFT, Gtk.ShadowType.NONE))
+        box.add(left_arrow)
+
+        right_arrow = Gtk.Button()
+        right_arrow.add(Gtk.Arrow(Gtk.ArrowType.RIGHT, Gtk.ShadowType.NONE))
+        box.add(right_arrow)
+
+        header_bar.pack_start(box)
+        self.add(header_bar)
+
+
+win = MainWindow()
+win.connect('delete-event', Gtk.main_quit)
+win.show_all()
+Gtk.main()
